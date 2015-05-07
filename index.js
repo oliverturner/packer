7fbc95ed18849f42b8fed3b5f4ce57ff557c27c9
Oliver Turner <oliver.turner@ticketmaster.co.uk>
2015-05-07 12:33:58 +0100

    Checking that changes were pushed

--- index.js
+++ index.js
@@ -38,10 +38,8 @@ function getLoaders(paths) {
       loaders: ['json']
     },
     expose: {
-      react: {
-        test: require.resolve('react'),
-        loader: 'expose?React'
-      }
+      test: require.resolve('react'),
+      loader: 'expose?React'
     },
     sass: {
       test: /\.scss$/,
