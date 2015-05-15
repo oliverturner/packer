2fc2e97401acea40a002f49c041f440a252e3491
Oliver Turner <oliver.turner@ticketmaster.co.uk>
2015-05-15 18:56:57 +0100

    publish update

---
+++ dist/index.js
@@ -0,0 +1,120 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+// Return a Webpack config
+
+var _autoprefixerCore = require('autoprefixer-core');
+
+var _autoprefixerCore2 = _interopRequireDefault(_autoprefixerCore);
+
+var _utilsGetLoaders = require('./utils/getLoaders');
+
+var _utilsGetLoaders2 = _interopRequireDefault(_utilsGetLoaders);
+
+var _utilsGetPlugins = require('./utils/getPlugins');
+
+var _utilsGetPlugins2 = _interopRequireDefault(_utilsGetPlugins);
+
+var _utilsUpdateOutput = require('./utils/updateOutput');
+
+var _utilsUpdateOutput2 = _interopRequireDefault(_utilsUpdateOutput);
+
+// Exported utils
+
+var _utilsGetEntries = require('./utils/getEntries');
+
+var _utilsGetEntries2 = _interopRequireDefault(_utilsGetEntries);
+
+var _utilsGetServerOpts = require('./utils/getServerOpts');
+
+var _utilsGetServerOpts2 = _interopRequireDefault(_utilsGetServerOpts);
+
+var env = process.env.NODE_ENV || 'development';
+var isProd = env === 'production';
+
+function validateOptions(options, requiredKeys) {
+  if (!options) {
+    throw new Error('options may not be omitted');
+  }
+
+  requiredKeys.map(function (key) {
+    if (!options[key]) throw new Error('options.' + key + ' may not be omitted');
+  });
+}
+
+// Export
+//-----------------------------------------------
+// Options:
+// * entry:     file, directory or array of entry points
+//
+// * jsUrl: path to JS files relative to webRoot
+// * srcs:  absolute paths to jsx, scss, etc
+/**
+ * @param options {{
+ *   entry:  string|[]
+ *   output: {
+ *     path:          string
+ *     publicPath:    string
+ *     filename:      string
+ *     chunkFilename: string
+ *   }
+ * }}
+ * @param {string} jsUrl
+ * @param {string} cssUrl
+ * @param {string} sassPath
+ *
+ * @returns {{
+ *  entry:  string|[]
+ *  output: {
+ *     publicPath: string
+ *     path:       string
+ *     filename:   string
+ *   },
+ *   module: {
+ *     loaders: []
+ *   },
+ *   plugins: [],
+ *   resolve: {},
+ *   postcss: {}
+ * }}
+ */
+
+var WebPacker = function WebPacker(options, jsUrl, cssUrl, sassPath) {
+  _classCallCheck(this, WebPacker);
+
+  validateOptions(options, ['entry', 'output']);
+
+  // Fill any missing optional values for `output` with defaults
+  options.output = (0, _utilsUpdateOutput2['default'])(options.output, jsUrl);
+
+  this.options = _extends({
+    module: {
+      loaders: (0, _utilsGetLoaders2['default'])(isProd, sassPath)
+    },
+
+    plugins: (0, _utilsGetPlugins2['default'])(isProd, jsUrl, cssUrl),
+
+    resolve: {
+      extensions: ['', '.js', '.jsx', '.json']
+    },
+
+    postcss: {
+      defaults: [_autoprefixerCore2['default']]
+    }
+  }, options);
+
+  return this.options;
+};
+
+exports['default'] = WebPacker;
+exports.getEntries = _utilsGetEntries2['default'];
+exports.getServerOpts = _utilsGetServerOpts2['default'];
---
+++ dist/utils/getLoaders.js
@@ -0,0 +1,75 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _extractTextWebpackPlugin = require('extract-text-webpack-plugin');
+
+var _extractTextWebpackPlugin2 = _interopRequireDefault(_extractTextWebpackPlugin);
+
+var _getLoader = require('./getLoader');
+
+var _getLoader2 = _interopRequireDefault(_getLoader);
+
+// Return an array of loaders for the various file types
+// In production we remove the ReactHotLoader and Sass plugins
+/**
+ * @param {bool} isProd
+ * @param {string} sassPath
+ *
+ * @returns {Array}
+ */
+function getLoaders(isProd, sassPath) {
+  var loaders = undefined,
+      sassLoaders = undefined;
+
+  sassLoaders = ['css', 'postcss', '@oliverturner/sass?includePaths[]=' + sassPath];
+  sassLoaders = sassLoaders.map(_getLoader2['default']).join('!');
+
+  loaders = {
+    json: {
+      test: /\.json$/,
+      loaders: ['json']
+    },
+    expose: {
+      test: require.resolve('react'),
+      loader: 'expose?React'
+    },
+    sass: {
+      test: /\.scss$/,
+      loader: sassLoaders
+    },
+    jsx: {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loaders: ['react-hot', 'babel']
+    }
+  };
+
+  // Production overrides
+  if (isProd) {
+    loaders = _extends(loaders, {
+      sass: {
+        test: /\.scss$/,
+        loader: _extractTextWebpackPlugin2['default'].extract('style-loader', sassLoaders)
+      },
+      jsx: {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel']
+      }
+    });
+  }
+
+  return Object.keys(loaders).map(function (key) {
+    return loaders[key];
+  });
+}
+
+exports['default'] = getLoaders;
+module.exports = exports['default'];
